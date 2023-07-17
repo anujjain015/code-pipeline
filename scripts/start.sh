7232dfe6c998a075e9f14801a7094cc80f4fccae
anujjain015 <64309441+anujjain015@users.noreply.github.com>
2023-07-17 17:39:55 +0530

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -7,7 +7,7 @@ sudo chmod -R 777 /home/ec2-user/express-app
 cd /home/ec2-user/express-app
 
 #install node modules
-pip install -r requirements.txt
+pip3 install -r requirements.txt
 
 #start our python app in the background
 FLASK_APP=app.py flask run --host=0.0.0.0
