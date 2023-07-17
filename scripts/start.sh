e0cc1c8ef64ad4608418b0872325e93e020b7e4b
anujjain015 <64309441+anujjain015@users.noreply.github.com>
2023-07-17 17:31:10 +0530

    Create start.sh

---
+++ scripts/start.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#give permission for everything in the express-app directory
+sudo chmod -R 777 /home/ec2-user/express-app
+
+#navigate into our working directory where we have all our github files
+cd /home/ec2-user/express-app
+
+#install node modules
+pip install -r requirements.txt
+
+#start our python app in the background
+FLASK_APP=app.py flask run --host=0.0.0.0
